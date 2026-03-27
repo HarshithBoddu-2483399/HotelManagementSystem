83fbdf84a930dca6dfbd8d682d05bd5310f4066e
Gandrakoti Dinesh <Gandrakoti.Dinesh@cognizant.com>
2026-03-27 09:37:51 +0530

    Updated My Controller , views and Model

--- HotelManagementSystem/Services/IBillingService.cs
+++ HotelManagementSystem/Services/IBillingService.cs
@@ -1,6 +1,11 @@
-﻿namespace HotelManagementSystem.Services
+﻿using System.Collections.Generic;
+using HotelManagementSystem.Models;
+
+namespace HotelManagementSystem.Services
 {
     public interface IBillingService
     {
+        IEnumerable<Invoice> GetInvoices();
+        Invoice ProcessCheckout(int reservationId);
     }
 }
